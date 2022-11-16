31960a63fc5650ff4097a7d8997d4f185b18f1b2
Mckz33 <mackenzie.max.rj@gmail.com>
2022-11-16 12:00:22 -0300

    TryCatch v1.1 String

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,15 +34,11 @@ public class Program {
             System.out.print("Check-in date (dd/MM/yyyy): ");
             checkOut = sdf.parse(input.next());
 
-            Date now = new Date();
-            if (checkIn.before(now) || checkOut.before(now)){
-                System.out.println("Error in reservation: Reservation dates for update must be future dates");
-            }
-            else if (!checkOut.after(checkIn)) {
-                System.out.println("Error in reservation: check-out date must be after check-in date");
+            String error = reservation.updateDates(checkIn, checkOut);
+            if (error != null){
+                System.out.println("Error in reservation: " + error);
             }
             else {
-                reservation.updateDate(checkIn, checkOut);
                 System.out.println("Reservation: " + reservation);
             }
 
